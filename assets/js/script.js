5ecfe56c7b03907019ae7e5bfdfbd1685fda08e4
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-09-13 16:12:45 -0700

    Beginning to write object code for questions for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var countdownEl = document.querySelector("#countdownClock")
 var highScoresListed = document.querySelector("#high-scores-listed")
 var playerInitialsListed = document.querySelector("player-initials-listed");
 var startButton = document.querySelector("#start-button");
+var question = document.querySelector("#question");
+var answer1Grab = document.querySelector("#answer1");
 
 var timer
 var timerCountdown
@@ -10,8 +12,24 @@ var storedPlayerInitials
 var storedHighScores
 var playerInitials
 var highScores
+var question
+var answer1
+var answer2
+var answer3
+var answer4
 
+var quizQuestions = [
+    {question: "Sample Question",
+    answer1: "Sample Answer 1",
+    answer2: "Sample Answer 2",
+    answer3: "Sample Answer 3",
+    answer4: "Sample Answer 4",
+    correctanswer: "Sample Answer 1"}
+];
 
+question.textContent = quizQuestions.question;
+answer1Grab.textContent = quizQuestions.answer1;
+answer1Grab.setAttribute("data-answer", quizQuestions.answer1);
 
 function init(){
     getplayerInitials();
