1bbf1886469475e4d97e4469972bbc3dab6c2922
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-09-16 01:24:56 -0700

    Added explanatory commentary to Javascript

--- assets/js/scorescript.js
+++ assets/js/scorescript.js
@@ -3,10 +3,10 @@ var playerInitialsListed = document.querySelector("#player-list");
 var highScoreList = document.querySelector("#high-scores-list");
 var formList = document.querySelector("list-activate");
 var resetButton = document.querySelector(".reset-button");
-
+//These are arrays which will hold the players and scores and reference local storage for both saving and deploying the data to and from local storage.
 var players = [];
 var scores = [];
-
+//This function starts by getting the player initials and score from local storate. If there are values to grab, the render functions will put them into lists on the High Scores page.
 function iniths(){
     getPlayerInitials();
     getHighScores();
@@ -16,7 +16,7 @@ function iniths(){
         renderScore();
     }
 };
-
+//This function will look for the key playerInitials in local storage and parse out with JSON any data it finds and put in the players array.
 function getPlayerInitials(){
     var playerInitials = JSON.parse(localStorage.getItem("playerInitials"));
     // console.log(playerInitials);
@@ -24,14 +24,14 @@ function getPlayerInitials(){
         players = playerInitials;
     };
 };
-
+//This function will look for the key highScores in local storage and parse out with JSON any data it finds and put in the scores array.
 function getHighScores(){
     var highScores = JSON.parse(localStorage.getItem("highScores"));
     if (highScores !==null){
         scores = highScores;
     };
 };
-
+//This will take the local storage data, loop through the array, and put the corresponding player name onto the High Scores list with an appended list element, giving each player their own list element to display their name.
 function renderPlayers() {
     playerInitialsListed.innerHTML = "";
     // console.log(players);
@@ -45,7 +45,7 @@ function renderPlayers() {
         playerInitialsListed.appendChild(li);
     };
 };
-
+//This will take the local storage data, loop through the array, and put the corresponding score onto the High Scores list with an appended list element, giving each player their own list element to display their score.
 function renderScore() {
     highScoreList.innerHTML = "";
     for (var i = 0; i < scores.length; i++) {
@@ -58,22 +58,18 @@ function renderScore() {
         highScoreList.appendChild(li);
     };
 };
-
+//This function uses JSON to stringify the player initals on the page and put them into local storage using the setItem command.
 function storePlayers(){
     localStorage.setItem("playerInitials", JSON.stringify(players));
 };
-
+//This function uses JSON to stringify the score on the page and put them into local storage using the setItem command.
 function storeScore(){
     localStorage.setItem("highScores", JSON.stringify(scores));
 };
-
-function resetPlayers(){
+//This function clears out any local storage in reguards to players and scores saved and wipes it clear.
+function resetScores(){
     localStorage.clear();
 };
-
-function resetScore(){
-    localStorage.clear();
-}
 
 resetButton.addEventListener("click", function (event){
     getPlayerInitials();
@@ -82,8 +78,7 @@ resetButton.addEventListener("click", function (event){
     renderScore();
     storePlayers();
     storeScore();   
-    resetPlayers();
-    resetScore();
+    resetScores();
 });
 iniths();
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ var answer3Grab = document.querySelector("#answer3");
 var answer4Grab = document.querySelector("#answer4");
 var answer5Grab = document.querySelector("#answer5");
 var correctIncorrect = document.querySelector("#correctIncorrect");
-
+//These are arrays which will hold our local storage data and pull them when looking at the high score sheet. They are empty now but are populated later with the .push command
 var players = [];
 var scores = [];
 
@@ -22,7 +22,7 @@ var winner = false
 var playerInitials
 var highScores = 0
 var currentQuestion = 0
-
+//This is an object which will cycle through the follow list of quesitons and answers, reading through the array. This will assign the appropriate question and answer to its corresponding HMTL tags
 var quizLayout = {
     question: "quizQuestionsArray[i]",
     answer1: "overallAnswersArray[i].quizAnswers[0]", 
@@ -31,10 +31,10 @@ var quizLayout = {
     answer4: "overallAnswersArray[i].quizAnswers[3]", 
     answer5: "overallAnswersArray[i].quizAnswers[4]"
     };
-
+//This is the question array which will be cycled through to match with the corresponding answer array
 var quizQuestionsArray = ["What does HTML stand for?", "Cascading Style Sheets are read in what order?", "What is the Javascript file extension?", "What command do we use to add attributes in Javascript?", "If we want to add an element to the end another element in Javascript, we use which command?", 
 "What is an object in Javascript?", "What does JSON stand for?", "What are semantic elements in HTML?", "What are some examples of assets in your code editor?", "Are you having fun with this quiz yet?"];
-
+//This is the answer array of arrays, the answers will cycle within their own arrays and find their appropriate HTML spot in regards to their number in the index
 var overallAnswersArray = [
 ["Hyper Typer Making Labels", "Hyper Text Markup Language", "How To Miss Lunch", "Hypo Textual Marked Language", "Hyper Text Markup Language"], 
 ["Top-to-Bottom", "Side-to-Side", "From the bottom up", "With a weathered eye", "Top-to-Bottom"],
@@ -48,7 +48,7 @@ var overallAnswersArray = [
 ["Yes", "Yes", "Yes", "Yes", "Yes"],
 ];
 
-
+//This function will start our quiz, set the win condition to false for the game to not finish early, set a countdown timer of 90 seconds in which to answer all the questions, it will hide the start button and reveal the section element containing the question buttons. It will then run the quiz function.
 function startQuiz() {
     winner = false;
     timerCountdown = 90;
@@ -57,7 +57,7 @@ function startQuiz() {
     sectionEl.removeAttribute("class", "hidden");
     quiz();
 };
-
+//This is the countdown clock function, it will start at 90 and then remove 1 second and update itself. If the timer is above 0 it will check to see if the win condition is met, otherwise, if it gets below zero, the game is over and the timer stops
 function countdownClock() {
     timer = setInterval(function () {
         timerCountdown--;
@@ -71,7 +71,7 @@ function countdownClock() {
     }, 1000); 
 
 };
-
+// This is the quiz function, it is the logic which cycles through the arrays and places the corresponding questions and answers into their html elements. For example, answer1 comes from the quizLayout object and is assigned the answer from the current array and the first variable in the index of that answer array. It also assigns a data-answer tag to each one to track which button the user clicks and how it corresponds to the current answers.
 function quiz(){
 
     quizLayout.question = quizQuestionsArray[currentQuestion]
@@ -92,7 +92,7 @@ function quiz(){
     answer4Grab.textContent = (quizLayout.answer4);
     answer4Grab.setAttribute("data-answer", quizLayout.answer4)
 };
-
+//This function checks to see if the player has won if the high score is above 0, which means they got one right, and that they finish all 10 questions before the timer runs out.
 function CheckWin(){
     if(highScores>0 && currentQuestion === 10){
         winner = true;
@@ -102,14 +102,14 @@ function CheckWin(){
         // renderScore();
     };
 };
-
+//This function pulls the player initials out of local storage and parses them with JSON. If there are initials in storage, this will pull them out and assign it the players variable
 function getPlayerInitials(){
     var playerInitials = JSON.parse(localStorage.getItem("playerInitials"));
     if (playerInitials !==null){ 
         players = playerInitials;
     };
 };
-
+//This function pulls the high scores out of local storage and parses them with JSON. If there are high scores in storage, this will pull them out and assign it the scores variable.
 function getHighScores(){
     var highScores = JSON.parse(localStorage.getItem("highScores"));
     if (highScores !==null){
@@ -117,17 +117,17 @@ function getHighScores(){
     };
 };
 
-
+//This function records the players variable into local storage using JSON stringify to put it into a format that local storage can read.
 function storePlayers(){
     localStorage.setItem("playerInitials", JSON.stringify(players));
 };
-
+//This function records the scores variable into local storage using JSON stringify to put it into a format that local storage can read.
 function storeScore(){
     localStorage.setItem("highScores", JSON.stringify(scores));
 };
-
+//This event listener corresponds to the start button, and when clicked on, will run the start Quiz function
 startButton.addEventListener("click", startQuiz);
-
+//This event listener has several factors going on. Upon clicking the elements with the card class will cyctle through their respective answers through the data-answer attribute using class and display hidden. If the answer equals the last answer in the answer array (which is hidden from the user) then it will register as correct, add 10 points to your score, and change the correct/incorrect paragraph element to green with the text correct. Otherwise, if you guessed a different button then it will compare to the hidden answer, and subtract 15 seconds from the countdown timer while changing the correct/incorrect text to wrong answer and the background color will turn red. It will then cycle through the array and check to see if it is the last question. If it was not the last question, it will go through the quiz function again and again until it is the last question, which  it will then change the elements to that of a form to fill out with the score you earned.
 sectionEl.addEventListener("click", function(event) {
     var element = event.target;
         var element = event.target;
@@ -156,6 +156,7 @@ sectionEl.addEventListener("click", function(event) {
             };
         };
     });
+//This addEventListener is the form which the player inputs their initials to be stored in local storage and displayed on the High Scores page. It will alert the user if they dont enter initials. It will also push the players and scores into their respective arrays to be kept in local storage through the use of the store and get functions.
     highScoreForm.addEventListener("submit", function(event){
         event.preventDefault();
         var initials = playerText.value
